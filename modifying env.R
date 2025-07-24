a36ec94366f91431758e0ded4a6d7af85c85bfbf
Philip <philip.faure13@gmail.com>
2025-07-24 15:10:52 +0200

    i messed up. I fixed it. peace.

--- modifying env.R
+++ modifying env.R
@@ -8,6 +8,7 @@ dates <- seq.Date(start_date, by = "month", length.out = n_months)
 # Add month column as the first column
 env <- data.frame(Month = format(dates, "%Y-%m"), env)
 env$Month <- as.POSIXct(env$Month, format = "%Y-%m", tz="Africa/Johannesburg")
+env$Date <- env$Month
 
 gannet$StartDate <- as.POSIXct(gannet$StartDate, format = "%Y-%m-%d")
 
@@ -15,7 +16,9 @@ gannet$Date <- format(gannet$StartDate, "%Y-%m")
 
 dates_counts <- unique(gannet$Date) %>% as.character()
 
-new_env <- env %>% 
-  filter(Month %in% dates_counts)
+new_env <- gannet %>% 
+  select(Date) %>% 
+  left_join(env, by = "Date")
+  
 
 write.csv(new_env, "./project_data/new_env.csv")
